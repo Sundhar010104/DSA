80599a87aa7c34365c9b3f7d8b03e4c94783b8f2
Sundharesan KP <126154812+Sundhar010104@users.noreply.github.com>
2025-12-05 21:46:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkEqual(self, a, b) -> bool:
+        a.sort()
+        b.sort()
+        if a == b:
+            return True
+            
+        return False
+        
