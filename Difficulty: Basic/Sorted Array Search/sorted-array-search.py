b7d410016fd700f5dae4ee30ab0906de837b60aa
Sundharesan KP <126154812+Sundhar010104@users.noreply.github.com>
2025-12-18 15:06:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted Array Search/sorted-array-search.py
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.py
@@ -3,5 +3,15 @@
 class Solution:
     ##Complete this function
     def searchInSorted(self,arr, k):
-        if k in arr:
-            return True
+        l = 0
+        r = len(arr)-1
+        while l<=r:
+            m = (l+r)//2
+            if k == arr[m] :
+                return True 
+            elif k > arr[m] :
+                l = m + 1
+            elif k < arr[m] :
+                r = m -1
+        return False
+    
